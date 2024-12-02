4cc1bf7fe24ade691ee6d59e8aebd524f69a0216
Simon Farshid <simon.farshid@outlook.com>
2024-12-01 20:45:38 -0800

    feat(fe): add data endpoint for charts (#20)

--- client-poc/lib/reltaApi.ts
+++ client-poc/lib/reltaApi.ts
@@ -1,15 +1,26 @@
-const MOCK_BASE_URL = "http://localhost:3002/api/mock";
 const BASE_URL =
   "http://github-assistant-alb-1250589316.us-east-1.elb.amazonaws.com";
 
-export const getDataQuery = async (query: string) => {
-  const response = await fetch(`${MOCK_BASE_URL}/dataQuery`, {
-    method: "POST",
-    body: JSON.stringify({ query }),
-  });
-  const { rows } = (await response.json()) as { rows: object[] };
-  return rows;
+export const getDataQuery = async (
+  org: string,
+  repo: string,
+  prompt: string
+) => {
+  const response = await fetch(
+    `${BASE_URL}/data?org=${org}&repo_name=${repo}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    }
+  );
+  const { sql_result } = (await response.json()) as { sql_result: object[] };
+  return sql_result ?? [];
 };
+
 export const getTextQuery = async (
   org: string,
   repo: string,
