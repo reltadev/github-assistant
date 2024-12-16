041b07e8fa9c472c0860c8c3cdb5c5dd68292700
Simon Farshid <simon.farshid@outlook.com>
2024-12-16 10:04:04 -0800

    feat: add posthog

--- client-poc/app/providers.tsx
+++ client-poc/app/providers.tsx
@@ -8,6 +8,8 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 let browserQueryClient: QueryClient | undefined = undefined;
 
@@ -22,12 +24,26 @@ export function getQueryClient() {
   }
 }
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+export function CSPostHogProvider({ children }) {
+  return { children };
+}
+
 export default function Providers({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
 
   return (
     <ClerkProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <PostHogProvider client={posthog}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </PostHogProvider>
     </ClerkProvider>
   );
 }
