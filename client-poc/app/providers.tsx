3b0c62b274ab911b527a981e9ea78f51f360cdb5
Simon Farshid <simon.farshid@outlook.com>
2024-12-16 18:07:27 -0800

    feat(fe): use reverse proxy

--- client-poc/app/providers.tsx
+++ client-poc/app/providers.tsx
@@ -26,7 +26,8 @@ export function getQueryClient() {
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
